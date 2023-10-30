c291169c28ad08df451c27547e2d87252c6bd98f
reganrone <regan.rone170@topper.wku.edu>
2023-10-30 11:56:20 -0500

    Timer
    Timer

--- Sonic The Hog/Assets/Scripts/TimerController.cs
+++ Sonic The Hog/Assets/Scripts/TimerController.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TimerController : MonoBehaviour
 {
-    public Text timerText; // Reference to the Text UI element
+    public TMP_Text timerText; // Reference to the Text UI element
     public float startTime = 0f; // Initial time for the timer
     private float currentTime = 0f;
-    private bool isRunning = false;
+    private bool isRunning = true;
 
     void Start()
     {
@@ -16,12 +17,11 @@ public class TimerController : MonoBehaviour
         UpdateTimerText();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (isRunning)
         {
             currentTime += Time.deltaTime; // Increment the timer
-
             // Update the text to display the current time
             UpdateTimerText();
         }
