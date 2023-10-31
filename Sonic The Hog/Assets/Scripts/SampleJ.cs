b39758584e7d2d2b2bb5fd57e392c756ca14681b
jss67933 <jessica.mata077@topper.wku.edu>
2023-10-31 10:53:08 -0500

    newest commit

---
+++ Sonic The Hog/Assets/Scripts/SampleJ.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleJ : MonoBehaviour
+{
+
+    // I'm actually starting to get really frustated with visual studio
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("boo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
