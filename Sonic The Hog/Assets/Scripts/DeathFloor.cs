8c5ed156acbbb5e94828481e803b07d2a5c6f159
reganrone <regan.rone170@topper.wku.edu>
2023-11-14 20:23:35 -0600

    death floor

---
+++ Sonic The Hog/Assets/Scripts/DeathFloor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFloor : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.position;
+        }
+        
+    }
+
+
+
+}
