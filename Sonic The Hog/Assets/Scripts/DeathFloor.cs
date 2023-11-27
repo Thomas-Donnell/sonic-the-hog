d6763ccf74b51ad92996a81a82828f72ff471497
reganrone <regan.rone170@topper.wku.edu>
2023-11-27 12:29:57 -0600

    lives and deathfloor

--- Sonic The Hog/Assets/Scripts/DeathFloor.cs
+++ Sonic The Hog/Assets/Scripts/DeathFloor.cs
@@ -1,34 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DeathFloor : MonoBehaviour
 {
     public GameObject player;
     public Transform respawnPoint;
-   
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            GameManager.instance.LoseLife(); // Call LoseLife from GameManager
             player.transform.position = respawnPoint.position;
         }
-        
-    }
 
+    }
 
 
 }
